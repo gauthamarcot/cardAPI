613551d274ba71cb88f19bdfbc1ca6c846a0ae70
gauthamarcot <gauthamarcot@yahoo.com>
2021-02-14 19:54:05 +0530

    image processor init and 30 % done

--- controller/imagefilecontroller.py
+++ controller/imagefilecontroller.py
@@ -1,11 +1,10 @@
 from flask import request
 from processor import imageprocessor
-
 import app
-
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    print('file recived')
     print(request.files)
     # checking if the file is present or not.
     if 'file' not in request.files:
@@ -13,4 +12,5 @@ def upload_file():
 
     file = request.files['file']
     file.save("static/test.jpg")
+    imageprocessor.textextract("static/test.jpg")
     return "file successfully saved"
--- processor/imageprocessor.py
+++ processor/imageprocessor.py
@@ -1,5 +1,19 @@
+import os
+import re
+import io
 import cv2
-import json
-import tesseract
+import json as js
+import numpy as np
+from PIL import Image as img
+import pytesseract as pyte
 
 def textextract(file):
+    image = cv2.imread(file)
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    adap = cv2.adaptiveThreshold(grey, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    imgfile = "{}.png".format(os.getpid())
+    cv2.imwrite(imgfile, adap)
+    text = pyte.image_to_string(img.open(imgfile), lang='eng')
+    text_output = open('../output.txt', 'w', encoding='utf-8')
+    text_output.write(text)
+    print(text)
